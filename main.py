f5ea64ef2918d5f1f9f544b4a5eed1ebd24981b6
vialaky <vialaky@gmail.com>
2024-10-30 17:01:58 +0200

    Add football data. Read match data.

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import json
 with open('football.json-master/2010-11/at.1.json', 'r') as file:
     data = json.load(file)
 
-print(data)
+# print(data)
+
+for item in data['rounds']:
+    match = item['matches']
+    print(match[0])
